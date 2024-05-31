a1e943f527b39fab50a43975022a89b7b8f61b1a
HenkSeed <adaner@inbox.ru>
2024-05-31 20:00:56 +0700

    Starting JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+/* console.log('Hello, World!'); */
+const headerNavItems = document.querySelectorAll('.header__nav__item');
+console.log('headerNavItems: ', headerNavItems);
+
+const heroDropConsult = document.querySelector('.hero__drop__consult');
+console.log('heroDropConsult: ', heroDropConsult);
+
+const heroDropArtifacts = document.querySelector('.hero__drop__artifacts');
+console.log('heroDropArtifacts: ', heroDropArtifacts);
+
+headerNavItems.forEach((navItem) => {
+	navItem.addEventListener('click', (target) => {
+		target.preventDefault();
+		console.log('clicked');
+	});
+});
