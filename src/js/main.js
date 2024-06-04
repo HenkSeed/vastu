590710994fe2b922ac7cf43bd5348ef64b81f689
HenkSeed <adaner@inbox.ru>
2024-06-05 00:45:55 +0700

    Finished correct shown mistakes and animations of the burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ const headerNavItems = headerSection.querySelectorAll('.header__nav__item');
 const heroSection = document.querySelector('.hero');
 const takeDropMenus = heroSection.querySelectorAll('.hero__drop__menu');
 
-// Находим каждое выпадающее меню по его классу
+// Находим в nav каждое выпадающее меню по их классу
 const heroDropConsult = heroSection.querySelector('.hero__drop__consult');
 const heroDropArtifacts = heroSection.querySelector('.hero__drop__artifacts');
 
@@ -18,31 +18,86 @@ const darkBg = document.querySelector('.dark-bg');
 
 // Находим бургер-меню
 const darkBurgerMenu = darkBg.querySelector('.dark-bg__burger-menu');
-console.log('darkBurgerMenu: ', darkBurgerMenu);
 
 // Находим крест бургер-меню
 const darkBgBurgerMenuCross = darkBg.querySelector(
 	'.dark-bg__burger-menu__cross'
 );
 
-// Перебераем все элементы навигационного меню
-headerNavItems.forEach((navItem) => {
-	navItem.addEventListener('click', (event) => {
-		event.preventDefault();
-		// Открываем выпадающее меню, соответствующее клику
-		showDropMenu(event);
-	});
-});
+// Находим массив элементов (якорные ссылки) бургер-меню
+const burgerMenuItems = darkBurgerMenu.querySelectorAll(
+	'.dark-bg__burger-menu__item a'
+);
 
+// Находим в бургер-меню каждое выпадающее меню по их классу
+const darkBurgerConsult = darkBg.querySelector('.dark-bg__burger__consult');
+console.log('darkBurgerConsult: ', darkBurgerConsult);
+const darkBurgerArtifacts = darkBg.querySelector('.dark-bg__burger__artifacts');
+console.log('darkBurgerArtifacts: ', darkBurgerArtifacts);
+
+// Находим крестики закрытия подменю бургера
+const darkBurgerConsultCross = darkBurgerConsult.querySelector(
+	'.dark-bg__burger__consult__cross'
+);
+console.log('darkBurgerConsultCross: ', darkBurgerConsultCross);
+
+const darkBurgerArtifactstCross = darkBurgerArtifacts.querySelector(
+	'.dark-bg__burger__artifacts__cross'
+);
+console.log('darkBurgerArtifactstCross: ', darkBurgerArtifactstCross);
+
+findNavItems();
+findBurgerMenuItems();
 showBurgerMenu();
 hideBurgerMenu();
 
+// Перебираем все элементы бургер-меню
+function findBurgerMenuItems() {
+	burgerMenuItems.forEach((menuItem) => {
+		menuItem.addEventListener('click', (event) => {
+			event.preventDefault();
+			// Открываем выпадающее из бургера меню, соответствующее клику
+			showBurgerDropMenu(event);
+		});
+	});
+}
+
+// Обрабатываем нажатие пунктов, вызывающих бургер-подменю
+function showBurgerDropMenu(event) {
+	console.log('event.target: ', event.target.innerText);
+	// Если кликнули на "Консультации"
+	if (event.target.textContent.toLowerCase().trim() == 'консультации') {
+		darkBurgerConsult.classList.toggle('get-invisible');
+		darkBurgerConsult.classList.toggle('get-visible');
+
+		darkBurgerMenu.classList.add('get-invisible');
+		darkBurgerMenu.classList.remove('get-visible');
+		// heroDropConsult.classList.toggle('hero__drop__shown');
+		// heroDropConsult.classList.toggle('hero__drop__hidden');
+		console.log('Pressed CONSULT');
+	} else {
+		console.log('Not pressed CONSULT');
+		// Если кликнули на другой пункт меню навигации
+		// heroDropConsult.classList.remove('hero__drop__shown');
+		// heroDropConsult.classList.add('hero__drop__hidden');
+	}
+}
+
+// Перебераем все элементы навигационного меню
+function findNavItems() {
+	headerNavItems.forEach((navItem) => {
+		navItem.addEventListener('click', (event) => {
+			event.preventDefault();
+			// Открываем выпадающее меню, соответствующее клику
+			showDropMenu(event);
+		});
+	});
+}
+
 // Обрабатываем нажатие пунктов навигационного меню
 function showDropMenu(event) {
-	console.log('event.target: ', event.target.textContent);
-
 	// Если кликнули на "Консультации"
-	if (event.target.textContent.toLowerCase() == 'консультации') {
+	if (event.target.textContent.toLowerCase().trim() == 'консультации') {
 		heroDropConsult.classList.toggle('hero__drop__shown');
 		heroDropConsult.classList.toggle('hero__drop__hidden');
 	} else {
@@ -52,7 +107,7 @@ function showDropMenu(event) {
 	}
 
 	// Если кликнули на "Артефакты"
-	if (event.target.textContent.toLowerCase() == 'артефакты') {
+	if (event.target.textContent.toLowerCase().trim() == 'артефакты') {
 		heroDropArtifacts.classList.toggle('hero__drop__shown');
 		heroDropArtifacts.classList.toggle('hero__drop__hidden');
 	} else {
@@ -64,21 +119,25 @@ function showDropMenu(event) {
 	// Если кликнули на "О компании"
 	if (event.target.textContent.toLowerCase() == 'о компании') {
 		console.log('О компании');
+		window.location.href = 'about.html';
 	}
 
 	// Если кликнули на "Отзывы"
 	if (event.target.textContent.toLowerCase() == 'отзывы') {
 		console.log('Отзывы');
+		window.location.href = 'reviews.html';
 	}
 
 	// Если кликнули на "Новости"
 	if (event.target.textContent.toLowerCase() == 'новости') {
 		console.log('Новости');
+		window.location.href = 'news.html';
 	}
 
 	// Если кликнули на "Контакты"
 	if (event.target.textContent.toLowerCase() == 'контакты') {
 		console.log('Контакты');
+		window.location.href = 'contacts.html';
 	}
 }
 
