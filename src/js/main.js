d027e973f2f80dacd8e1f243ce63c42451e6d513
HenkSeed <adaner@inbox.ru>
2024-06-05 23:01:27 +0700

    Finished burger menues and create modules for js and start cart module

--- src/js/main.js
+++ src/js/main.js
@@ -1,87 +1,19 @@
+import burger from '/js/modules/burger.js';
+
 // Находим в шапке сайта пункты навигационного меню
 const headerSection = document.querySelector('.header');
 const headerNavItems = headerSection.querySelectorAll('.header__nav__item');
 
 // Находим в блоке hero массив выпадающих меню
 const heroSection = document.querySelector('.hero');
-const takeDropMenus = heroSection.querySelectorAll('.hero__drop__menu');
 
 // Находим в nav каждое выпадающее меню по их классу
 const heroDropConsult = heroSection.querySelector('.hero__drop__consult');
 const heroDropArtifacts = heroSection.querySelector('.hero__drop__artifacts');
 
-// Находим иконку бургера
-const headerBurger = headerSection.querySelector('.header__burger__icon');
-
-// Находим тёмное окно
-const darkBg = document.querySelector('.dark-bg');
-
-// Находим бургер-меню
-const darkBurgerMenu = darkBg.querySelector('.dark-bg__burger-menu');
-
-// Находим крест бургер-меню
-const darkBgBurgerMenuCross = darkBg.querySelector(
-	'.dark-bg__burger-menu__cross'
-);
-
-// Находим массив элементов (якорные ссылки) бургер-меню
-const burgerMenuItems = darkBurgerMenu.querySelectorAll(
-	'.dark-bg__burger-menu__item a'
-);
-
-// Находим в бургер-меню каждое выпадающее меню по их классу
-const darkBurgerConsult = darkBg.querySelector('.dark-bg__burger__consult');
-console.log('darkBurgerConsult: ', darkBurgerConsult);
-const darkBurgerArtifacts = darkBg.querySelector('.dark-bg__burger__artifacts');
-console.log('darkBurgerArtifacts: ', darkBurgerArtifacts);
-
-// Находим крестики закрытия подменю бургера
-const darkBurgerConsultCross = darkBurgerConsult.querySelector(
-	'.dark-bg__burger__consult__cross'
-);
-console.log('darkBurgerConsultCross: ', darkBurgerConsultCross);
-
-const darkBurgerArtifactstCross = darkBurgerArtifacts.querySelector(
-	'.dark-bg__burger__artifacts__cross'
-);
-console.log('darkBurgerArtifactstCross: ', darkBurgerArtifactstCross);
+burger();
 
 findNavItems();
-findBurgerMenuItems();
-showBurgerMenu();
-hideBurgerMenu();
-
-// Перебираем все элементы бургер-меню
-function findBurgerMenuItems() {
-	burgerMenuItems.forEach((menuItem) => {
-		menuItem.addEventListener('click', (event) => {
-			event.preventDefault();
-			// Открываем выпадающее из бургера меню, соответствующее клику
-			showBurgerDropMenu(event);
-		});
-	});
-}
-
-// Обрабатываем нажатие пунктов, вызывающих бургер-подменю
-function showBurgerDropMenu(event) {
-	console.log('event.target: ', event.target.innerText);
-	// Если кликнули на "Консультации"
-	if (event.target.textContent.toLowerCase().trim() == 'консультации') {
-		darkBurgerConsult.classList.toggle('get-invisible');
-		darkBurgerConsult.classList.toggle('get-visible');
-
-		darkBurgerMenu.classList.add('get-invisible');
-		darkBurgerMenu.classList.remove('get-visible');
-		// heroDropConsult.classList.toggle('hero__drop__shown');
-		// heroDropConsult.classList.toggle('hero__drop__hidden');
-		console.log('Pressed CONSULT');
-	} else {
-		console.log('Not pressed CONSULT');
-		// Если кликнули на другой пункт меню навигации
-		// heroDropConsult.classList.remove('hero__drop__shown');
-		// heroDropConsult.classList.add('hero__drop__hidden');
-	}
-}
 
 // Перебераем все элементы навигационного меню
 function findNavItems() {
@@ -118,57 +50,21 @@ function showDropMenu(event) {
 
 	// Если кликнули на "О компании"
 	if (event.target.textContent.toLowerCase() == 'о компании') {
-		console.log('О компании');
 		window.location.href = 'about.html';
 	}
 
 	// Если кликнули на "Отзывы"
 	if (event.target.textContent.toLowerCase() == 'отзывы') {
-		console.log('Отзывы');
 		window.location.href = 'reviews.html';
 	}
 
 	// Если кликнули на "Новости"
 	if (event.target.textContent.toLowerCase() == 'новости') {
-		console.log('Новости');
 		window.location.href = 'news.html';
 	}
 
 	// Если кликнули на "Контакты"
 	if (event.target.textContent.toLowerCase() == 'контакты') {
-		console.log('Контакты');
 		window.location.href = 'contacts.html';
 	}
-}
-
-// Показываем затемнённое окно
-function showDark() {
-	darkBg.classList.add('dark-bg__show');
-	document.body.style.overflow = 'hidden';
-}
-
-// Убираем затемнённое окно
-function hideDark() {
-	darkBg.classList.remove('dark-bg__show');
-	document.body.style.overflow = '';
-}
-
-// Вызываем бургер-меню
-function showBurgerMenu() {
-	headerBurger.addEventListener('click', (event) => {
-		console.log('event: ', event.target);
-		// Открываем бургер-меню в затемнённом окне
-
-		// Показываем затемнение окно
-		showDark();
-	});
-}
-
-function hideBurgerMenu() {
-	darkBgBurgerMenuCross.addEventListener('click', (event) => {
-		// Скрываем бургер-меню в затемнённом окне
-		// Убираем затемнённое окно
-
-		hideDark();
-	});
 }
---
+++ src/js/modules/burger.js
@@ -0,0 +1,183 @@
+function burger() {
+	// Находим шапку сайта для сокращения времени поиска элементов
+	const headerSection = document.querySelector('.header');
+
+	// Находим иконку бургера
+	const headerBurger = headerSection.querySelector('.header__burger__icon');
+
+	// Находим тёмное окно
+	const darkBg = document.querySelector('.dark-bg');
+
+	// Находим бургер-меню
+	const darkBurgerMenu = darkBg.querySelector('.dark-bg__burger-menu');
+
+	// Находим крест бургер-меню
+	const darkBgBurgerMenuCross = darkBg.querySelector(
+		'.dark-bg__burger-menu-cross'
+	);
+
+	// Находим массив элементов (якорные ссылки) бургер-меню
+	const burgerMenuItems = darkBurgerMenu.querySelectorAll(
+		'.dark-bg__burger-menu-item a'
+	);
+
+	// Находим в бургер-меню каждое выпадающее меню по их классу
+	const darkBurgerConsult = darkBg.querySelector('.dark-bg__burger-consult');
+	const darkBurgerArtifacts = darkBg.querySelector(
+		'.dark-bg__burger-artifacts'
+	);
+
+	// Находим крестики закрытия подменю Consult бургера
+	const darkBurgerConsultCross = darkBurgerConsult.querySelector(
+		'.dark-bg__burger-consult-cross'
+	);
+
+	// Находим строчку закрытия меню Consult
+	const closeConsultItem = darkBurgerConsult.querySelector(
+		'.dark-bg__burger-consult>a'
+	);
+
+	// Находим крестики закрытия подменю Artifacts бургера
+	const darkBurgerArtifactsCross = darkBurgerArtifacts.querySelector(
+		'.dark-bg__burger-artifacts-cross'
+	);
+
+	// Находим строчку закрытия меню Artifacts
+	const closeArtifactsItem = darkBurgerArtifacts.querySelector(
+		'.dark-bg__burger-artifacts>a'
+	);
+
+	// Отлавливаем нажатие на строку закрытия Artifacts меню
+	closeArtifactsItem.addEventListener('click', (event) => {
+		closeArtifactsMenu();
+	});
+
+	// Отлавливаем нажатие на крестик закрытия Artifacts меню
+	darkBurgerArtifactsCross.addEventListener('click', (event) => {
+		closeArtifactsMenu();
+	});
+
+	// Функция закрытия Artifacts меню
+	function closeArtifactsMenu() {
+		console.log('Нажат элемент закрытия меню артефактов');
+		darkBurgerArtifacts.classList.toggle('get-invisible');
+		darkBurgerArtifacts.classList.toggle('get-visible');
+
+		getBurgerMenuVisible();
+	}
+
+	// Отлавливаем нажатие на строку закрытия Consult меню
+	closeConsultItem.addEventListener('click', (event) => {
+		closeConsultMenu();
+	});
+
+	// Отлавливаем нажатие на крестик закрытия Consult меню
+	darkBurgerConsultCross.addEventListener('click', (event) => {
+		closeConsultMenu();
+	});
+
+	findBurgerMenuItems();
+
+	showBurgerMenu();
+	hideBurgerMenu();
+
+	// Функция закрытия Consult меню
+	function closeConsultMenu() {
+		console.log('Нажат элемент закрытия меню консультации');
+		darkBurgerConsult.classList.toggle('get-invisible');
+		darkBurgerConsult.classList.toggle('get-visible');
+
+		getBurgerMenuVisible();
+	}
+
+	// Перебираем все элементы бургер-меню
+	function findBurgerMenuItems() {
+		burgerMenuItems.forEach((menuItem) => {
+			menuItem.addEventListener('click', (event) => {
+				event.preventDefault();
+				// Открываем выпадающее из бургера меню, соответствующее клику
+				showBurgerDropMenu(event);
+			});
+		});
+	}
+
+	// Обрабатываем нажатие пунктов, вызывающих бургер-подменю
+	function showBurgerDropMenu(event) {
+		// Если кликнули на "Консультации"
+		if (event.target.textContent.toLowerCase().trim() == 'консультации') {
+			darkBurgerConsult.classList.toggle('get-invisible');
+			darkBurgerConsult.classList.toggle('get-visible');
+
+			getBurgerMenuInvisible();
+		}
+
+		// Если кликнули на "Артефакты"
+		if (event.target.textContent.toLowerCase().trim() == 'артефакты') {
+			darkBurgerArtifacts.classList.toggle('get-invisible');
+			darkBurgerArtifacts.classList.toggle('get-visible');
+
+			getBurgerMenuInvisible();
+		}
+
+		if (event.target.textContent.toLowerCase().trim() == 'о компании') {
+			window.location.href = 'about.html';
+		}
+
+		if (event.target.textContent.toLowerCase().trim() == 'отзывы') {
+			window.location.href = 'reviews.html';
+		}
+
+		if (event.target.textContent.toLowerCase().trim() == 'новости') {
+			window.location.href = 'news.html';
+		}
+
+		if (event.target.textContent.toLowerCase().trim() == 'контакты') {
+			window.location.href = 'contacts.html';
+		}
+	}
+
+	// Делаем основное бургер-меню невидимым
+	function getBurgerMenuInvisible() {
+		darkBurgerMenu.classList.add('get-invisible');
+		darkBurgerMenu.classList.remove('get-visible');
+	}
+
+	// Делаем основное бургер-меню видимым
+	function getBurgerMenuVisible() {
+		darkBurgerMenu.classList.remove('get-invisible');
+		darkBurgerMenu.classList.add('get-visible');
+	}
+
+	// Показываем затемнённое окно
+	function showDark() {
+		darkBg.classList.add('dark-bg--shown');
+		document.body.style.overflow = 'hidden';
+	}
+
+	// Убираем затемнённое окно
+	function hideDark() {
+		darkBg.classList.remove('dark-bg--shown');
+		document.body.style.overflow = '';
+	}
+
+	// Вызываем бургер-меню
+	function showBurgerMenu() {
+		headerBurger.addEventListener('click', (event) => {
+			// Открываем бургер-меню в затемнённом окне
+
+			// Показываем затемнение окно
+			showDark();
+		});
+	}
+
+	function hideBurgerMenu() {
+		darkBgBurgerMenuCross.addEventListener('click', (event) => {
+			// Скрываем бургер-меню в затемнённом окне
+			// Убираем затемнённое окно
+
+			hideDark();
+		});
+	}
+}
+
+export default burger;
