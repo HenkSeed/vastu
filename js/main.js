4d0407167937b3df31d2833e5379d71c906f6dd2
HenkSeed <adaner@inbox.ru>
2024-06-08 20:05:28 +0700

    Updates

--- js/main.js
+++ js/main.js
@@ -3,16 +3,16 @@ import burger from './burger.js';
 import cart from './cart.js';
 import headerDropMenu from './headerDropMenu.js';
 
-// Находим в шапке сайта пункты навигационного меню
-const headerSection = document.querySelector('.header');
-const headerNavItems = headerSection.querySelectorAll('.header__nav__item');
+// // Находим в шапке сайта пункты навигационного меню
+// const headerSection = document.querySelector('.header');
+// const headerNavItems = headerSection.querySelectorAll('.header__nav__item');
 
-// Находим в блоке hero массив выпадающих меню
-const heroSection = document.querySelector('.hero');
+// // Находим в блоке hero массив выпадающих меню
+// const heroSection = document.querySelector('.hero');
 
-// Находим в nav каждое выпадающее меню по их классу
-const heroDropConsult = heroSection.querySelector('.hero__drop__consult');
-const heroDropArtifacts = heroSection.querySelector('.hero__drop__artifacts');
+// // Находим в nav каждое выпадающее меню по их классу
+// const heroDropConsult = heroSection.querySelector('.hero__drop__consult');
+// const heroDropArtifacts = heroSection.querySelector('.hero__drop__artifacts');
 
 burger();
 
